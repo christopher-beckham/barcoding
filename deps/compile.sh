6c1afe460abf24c226575c8f2d67fbfa3ee887ca
Christopher Beckham <chrispy645@gmail.com>
2014-06-28 20:32:19 +1200

    Makefile changes and cygwin check for compile.sh

--- deps/compile.sh
+++ deps/compile.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-tar -vxzf argtable2-13.tar.gz
-tar -vxzf clustal-omega-1.2.1.tar.gz
+# binary release of clustalo caused segfaults, so on cygwin
+# we're compiling clustalo ourselves
+if [ "$(expr substr $(uname -s) 1 6)" == "CYGWIN" ]; then
+	tar -vxzf argtable2-13.tar.gz
+	tar -vxzf clustal-omega-1.2.1.tar.gz
 
-echo "Compiling argtable2-13..."
+	echo "Compiling argtable2-13..."
 
-cd argtable2-13
-./configure
-make
+	cd argtable2-13
+	./configure
+	make
 
-echo "Compiling clustalo..."
+	echo "Compiling clustalo..."
 
-cd ..
-cd clustal-omega-1.2.1
+	cd ..
+	cd clustal-omega-1.2.1
 
-./configure CFLAGS="-I"`realpath ../argtable2-13/src` LDFLAGS="-L"`realpath ../argtable2-13/src/.libs`
-make
+	./configure CFLAGS="-I"`realpath ../argtable2-13/src` LDFLAGS="-L"`realpath ../argtable2-13/src/.libs`
+	make
+else
+	echo "placeholder"
+fi
