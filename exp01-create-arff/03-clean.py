168582d3a1f37b20360158f97d93e39aefd94e51
Christopher Beckham <chrispy645@gmail.com>
2014-06-28 21:35:16 +1200

    More changes to Makefile

--- exp01-create-arff/03-clean.py
+++ exp01-create-arff/03-clean.py
@@ -2,13 +2,14 @@ from Bio import SeqIO
 from Bio import Seq
 
 import sys
+import StringIO
 
 """
 TODO: read from stdin instead?
 """
 
-omega_file = sys.argv[1]
+fstring = StringIO.StringIO( sys.stdin.read() )
 
-records = SeqIO.parse(open(omega_file), "fasta")
+records = SeqIO.parse(fstring, "fasta")
 for rec in records:
 	print rec.id + "," + rec.seq
