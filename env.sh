d974446aba050a9f8c5a9ea5dabdb97c2c7a476d
Christopher Beckham <chrispy645@gmail.com>
2014-07-13 11:11:48 +1200

    Moved out folder to be external, modified mafft params

--- env.sh
+++ env.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ -n $(command -v "parallel") ]; then
+	echo "yay"
+fi
+
 export CLUSTALO=`realpath deps/clustal-omega-1.2.1/src/clustalo.exe`
 export EXP01_NUCLIMIT=3000
 export EXP01_FRAGLEN=100 # use 0 if you don't want randomly sampled fragments!
+export OUT_FOLDER="/cygdrive/g/barcoding/output"
+export OUT_FOLDER_WIN="G:/barcoding/output"
--- exp01-create-arff/exp.sh
+++ exp01-create-arff/exp.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-for f in 100 200 300 400 500; do
-	echo "f = "$f
-	for i in {0..4}; do
-		echo "i = "$i
-		python 02-chop.py --fraglen=$f --seed=$i < output/res.fasta > output/res.fasta.$i.chop
-		mafft --thread 4 --auto output/res.fasta.$i.chop > output/res.$i.omega
-		python 03-clean.py < output/res.$i.omega > output/res.$i.clean
-		python 04-csv.py < output/res.$i.clean > output/res.$f.$i.csv
-		
-		rm output/res.fasta.$i.chop output/res.$i.omega output/res.$i.clean
-	done
-done
+f=$1
+i=$2
+
+echo "f = "$f
+echo "i = "$i
+
+# TODO: don't parallelize this
+
+python 02-chop.py --fraglen=$f --seed=$i < $OUT_FOLDER/res.fasta > $OUT_FOLDER/res.fasta.$f.$i.chop
+mafft --nomemsave --alga --parttree --retree 2 --partsize 1000 --thread 4 --inputorder $OUT_FOLDER/res.fasta.$f.$i.chop > $OUT_FOLDER/res.$f.$i.omega
+
+python 03-clean.py < $OUT_FOLDER/res.$f.$i.omega > $OUT_FOLDER/res.$f.$i.clean
+python 04-csv.py < $OUT_FOLDER/res.$f.$i.clean > $OUT_FOLDER/res.$f.$i.csv
+
+java -Xmx6000M weka.core.converters.CSVLoader -B `wc -l $OUT_FOLDER_WIN/res.$f.$i.csv` $OUT_FOLDER/res.$f.$i.csv > $OUT_FOLDER/res.$f.$i.arff
+
+rm $OUT_FOLDER/res.fasta.$f.$i.chop $OUT_FOLDER/res.$f.$i.omega $OUT_FOLDER/res.$f.$i.clean
