27a6662127ff7fc815819d28b9f9467134b466df
Christopher Beckham <chrispy645@gmail.com>
2014-07-25 15:26:02 +1200

    Made exp.sh verbose and updated env.sh

--- exp03-freq/env.sh
+++ exp03-freq/env.sh
@@ -7,13 +7,7 @@
 # For 01-csv.py
 
 export EXP03_MAX_CLASS=40
-
-# For gen-params.py
-
 export EXP03_KMER_MIN=3
 export EXP03_KMER_MAX=7
-export EXP03_KMER_STEP=1
 
-export EXP03_SEED_MIN=0
-export EXP03_SEED_MAX=4
-export EXP03_SEED_STEP=1
+
--- exp03-freq/exp.sh
+++ exp03-freq/exp.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/bash -v
 
-python 01-csv.py --kmer="3,7" --outfile=$OUT_FOLDER/out.csv --outlist=$OUT_FOLDER/out.list < res.fasta
+python 01-csv.py --kmer="3,7" --outfile=$OUT_FOLDER/out.csv --outlist=$OUT_FOLDER/out.list < $EXP_SHARED/res.fasta
 python 02-write-sh.py < out.list > weka.sh
 
 
