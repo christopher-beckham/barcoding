5b4c83f0496cf201ee2356933bd2073edc9d09e1
Christopher Beckham <chrispy645@gmail.com>
2014-08-16 22:50:21 +1200

    Move output folder

--- exp03-freq/exp.sh
+++ exp03-freq/exp.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "Using training file: "$OUT_FOLDER/$1.json
-
-python 01-csv.py --kmer="3,5" --taxlevel="genus" --outfile=$OUT_FOLDER/$1.csv --outlist=$OUT_FOLDER/$1.list < $OUT_FOLDER/$1.json
-#python 02-write-sh.py < $OUT_FOLDER/out2.list > weka.sh
-java -Xmx6000M weka.core.converters.CSVLoader -B `wc -l $OUT_FOLDER_WIN/$1.csv` $OUT_FOLDER_WIN/$1.csv > $OUT_FOLDER/$1.csv.arff
-#./weka.sh $OUT_FOLDER_WIN/out.csv.arff > out.csv.arff.results
-
+FILE="res50k"
+echo "Creating $FILE.arff from $FILE.json"
+python 01-csv-new.py --kmer="3,5" --taxlevel="genus" --outfile=$OUT_FOLDER/$FILE.arff --outlist=null --infile=$OUT_FOLDER/$FILE.json --maxclass=300
