2af978234b294cc87ea5329e4d0bf71174ab19dd
Christopher Beckham <chrispy645@gmail.com>
2014-10-10 22:32:07 +1300

    Small stuff to do with Porter folder mainly

---
+++ porter-datasets/porter-fasta2json.py
@@ -0,0 +1,32 @@
+import sys
+from Bio import SeqIO
+import argparse
+import json
+
+parser = argparse.ArgumentParser(description="Summarise the classes from a Porter FASTA file")
+parser.add_argument('--infile', dest='infile', required=True, help="FASTA input file")
+parser.add_argument('--taxlevel', dest='taxlevel', required=True, help="Taxonomic level (e.g. genus, family)")
+args = parser.parse_args()
+
+#classes = dict()
+
+json_arr = []
+f = open(args.infile)
+for record in SeqIO.parse(f, "fasta"):
+	clas = str(record.description).split(';')[-1]
+	#if clas not in classes:
+	#	classes[clas] = 0
+	#classes[clas] += 1
+	json_arr.append( {"nucid": "?", "taxinfo": {args.taxlevel: clas}, "fasta": str(record.seq).upper() } )
+	
+f.close()
+
+print json.dumps(json_arr, sort_keys=True, indent=4, separators=(',', ': '))
+
+"""
+for key in classes:
+	print key,
+	print classes[key]
+	
+print len(classes.keys())
+"""
