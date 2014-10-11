76d18a00a70d2c00fa81d54a32fe4b797e066a5e
Christopher Beckham <chrispy645@gmail.com>
2014-10-11 22:33:46 +1300

    Updated F-measures again, parse-measures now gets the avg. weighted F-measure

--- PAPER/parse-measures.py
+++ PAPER/parse-measures.py
@@ -2,25 +2,27 @@
 
 import sys
 
-F_MEASURE = 4
+F_MEASURE = 4 # the index of the f-measure column
 CLASS = 8
 metrics = []
 class_sizes = []
-
-print "f_measure,class,count"
 
 body = []
 for line in sys.stdin:
 	body.append( line.rstrip() )
 
+print "f_measure,class,count"	
+	
 body = body[ body.index("=== Detailed Accuracy By Class ===") + 3 :: ]
 for line in body:
 	if "Weighted Avg." in line:
+		line = line.split()
+		metrics.append( [ line[F_MEASURE+2], '"WEIGHTED F-MEASURE"' ] )
 		break
-	line = line.split()
-	if len(line) >= 5:
-		metrics.append( [ line[F_MEASURE], '"' + " ".join(line[CLASS::]) + '"' ] )
-
+	else:
+		line = line.split()
+		if len(line) >= 5:
+			metrics.append( [ line[F_MEASURE], '"' + " ".join(line[CLASS::]) + '"' ] )
 
 
 body = body[ body.index("=== Confusion Matrix ===") + 3 :: ]
@@ -29,6 +31,7 @@ for line in body:
 		break
 	line = line.split()
 	class_sizes.append( [ str(sum([ int(elem) for elem in line[0 : line.index('|') ] ])) ] )
+class_sizes.append(["0"])
 	
 for i in range(0, len(metrics)):
 	print ",".join( metrics[i] + class_sizes[i] )
