c7a3556e8f248ecf1b0712d308656e18d88d6840
Christopher Beckham <chrispy645@gmail.com>
2014-11-17 13:29:03 +1300

    Added cleaned res50k and ibol resulting from script remove-dupes.py

---
+++ exp-shared/remove-dupes.py
@@ -0,0 +1,49 @@
+import sys
+import StringIO
+import random
+import argparse
+import json
+import operator
+
+def err(st):
+	sys.stderr.write( str(st) )
+	sys.stderr.write("\n")
+
+entries = json.loads( sys.stdin.read() )
+
+strings_seen = set()
+new_records = []
+retained = 0
+dupe_count = 0
+for entry in entries:
+	if entry['fasta'] in strings_seen:
+		dupe_count += 1
+		continue
+	elif entry['fasta'] == "":
+		continue
+	else:
+		strings_seen.add( entry['fasta'] )
+		new_records.append(entry)
+		retained += 1
+err("Sequences ignored due to a previous duplicate: " + str(dupe_count))
+err("Number of sequences retained: " + str(retained))
+
+err("Getting rid of substrings...")
+substring_count = 0
+new_new_records = []
+
+sorted_seqs = sorted( [ entry['fasta'] for entry in new_records ], key=len )
+ignore_set = set()
+for x in range(0, len(sorted_seqs)):
+	for y in range(0, x):
+		if len(sorted_seqs[y]) >= sorted_seqs[x]:
+			break
+		elif sorted_seqs[y] in sorted_seqs[x]:
+			ignore_set.add( sorted_seqs[y] )
+			substring_count += 1
+for entry in new_records:
+	if entry['fasta'] not in ignore_set:
+		new_new_records.append(entry)		
+err("Number of substrings removed: " + str(substring_count))
+
+print json.dumps(new_new_records, sort_keys=True, indent=4, separators=(',', ': '))
