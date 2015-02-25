a4546ab2caebd6414b0d27297507a8e9443bdadf
Christopher Beckham <chrispy645@gmail.com>
2015-02-26 12:02:40 +1300

    More stuff

---
+++ PAPER/experiments/kmers-16/copy-figures.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp figure1-arff-file.png ~/Desktop/Dropbox/comp390/latex/figure1.png
+cp workflow.pdf ~/Desktop/Dropbox/comp390/latex/figure2.pdf
+cp tally-classes/figure-classdist.pdf ~/Desktop/Dropbox/comp390/latex/figure3.pdf
+cp results/feb5-graph.pdf ~/Desktop/Dropbox/comp390/latex/figure4.pdf
+cp f-measure/f-measures-histogram.pdf ~/Desktop/Dropbox/comp390/latex/figure5.pdf
